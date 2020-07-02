1a459fcd8567602a8056ac3bfe5d1d5b13724a8f
ThevinSilva <61995848+ThevinSilva@users.noreply.github.com>
2020-07-02 19:20:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const socketio =require('socket.io');
+const http = require('http');
+const router = require('./router')
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const {addUser, removeUser, getUser, getUserInRoom} = require( './users.js')
+
+app.use(router)
+app.use(cors)
+
+
+io.on('connection', (socket)=>{
+    socket.on('join', ({name, room},callback) => {
+        const{error, user } = addUser({ id: socket.id, name, room});
+
+        if(error) return callback(error);
+
+        socket.emit('message', {user:'admin', text:`${user.name}, welcome to ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`})
+
+        socket.join(user.room);
+
+        console.log(user.name)
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message,callback) =>{
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', {user:user.name, text:message});
+
+        callback();
+    })
+
+    
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if (user){
+            io.to(user.room).emit('message',{user:'admin',text:`${user.name} has left.`})
+        }
+
+    })
+})
+
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
+
